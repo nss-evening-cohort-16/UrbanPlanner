4c24ffc01b4ca23720c18ddd054b1afa94fe3c93
Brian Neal <brianbneal@gmail.com>
2022-01-12 08:12:33 -0600

    Add project files.

---
+++ UrbanPlanner/Models/Building.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner.Models
+{
+    class Building
+    {
+        //overloading - multiple methods with same name but different signatures
+        public Building()
+        {
+
+        }
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        //fields
+        private string _designer = "Brian Neal";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        
+        //properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get => Width * Depth * (3 * Stories);
+        }
+
+        //methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string name)
+        {
+            _owner = name;
+        }
+        public void PrintToConsole()
+        {
+            //@ - string verbatim - will take every keystroke literally including new lines (Enter key)
+            string output = $@"
+{_address}-------
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space";
+            
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UrbanPlanner.Models;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate - use the 'new' keyword to create an instance of an object
+            //initialize - give an instance an initial value
+            Building oneTwoThree = new Building("123 Main Street");
+            oneTwoThree.Width = 5;
+            oneTwoThree.Stories = 2;
+            oneTwoThree.Depth = 10;
+
+            Building fourFiveSix = new Building("456 Main Street")
+            {
+                Width = 7,
+                Depth = 4,
+                Stories = 3
+            };
+
+
+            Building differentOne = new("1023 Something Avenue")
+            {
+                Width = 6,
+                Depth = 15,
+                Stories = 1
+            };
+
+
+            Building sherlocksPlace = new Building("221B Baker Street")
+            {
+                Width = 10,
+                Depth = 10,
+                Stories = 10
+            };
+            oneTwoThree.Construct();
+            fourFiveSix.Construct();
+            sherlocksPlace.Construct();
+            differentOne.Construct();
+            
+            oneTwoThree.Purchase("Scrooge McDuck");
+            oneTwoThree.PrintToConsole();
+            Console.ReadLine();
+            
+        }
+    }
+}
